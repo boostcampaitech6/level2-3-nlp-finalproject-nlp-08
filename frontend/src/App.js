3c9e2334c3fdcaad59758b20e40fd89275dc5584
Nbecause <thdduddn9696@gmail.com>
2024-02-19 07:42:30 +0900

    add: box description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* Description text */}
+      <div className='box-description'>
+        <p>분석하고 싶은 글을 입력해주세요</p>
+      </div>
+
       {/* 중앙 텍스트 박스 */}
       <div className="center-container">  
         <textarea
