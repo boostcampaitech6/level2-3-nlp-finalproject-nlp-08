a491d535322ff5eb076658269cf5aa4f6ae84ed6
Nbecause <thdduddn9696@gmail.com>
2024-02-19 09:50:58 +0900

    feature: mainpage button functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,27 @@ import NormalButton from './components/NormalButton';
 import InputTextBox from './components/InputTextBox';
 
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+
+  const handleSendTextClick = () => {
+    console.log('Send Text Button clicked');
+  };
+
+  const handleDeleteTextClick = () => {
+    setText('');
+  };
 
   return (
     <div className="App">
-      {/* Description text */}
       <div className='box-description'>
         <p>분석하고 싶은 글을 입력해주세요</p>
       </div>
 
-      {/* 중앙 텍스트 박스 */}
       <InputTextBox value={text} onChange={(e) => setText(e.target.value)} />
 
-      {/* 버튼 */}
       <div className="mainpage-button-container">
-          <NormalButton label="분석하기" onClick={() => console.log('Button 1 clicked')} />
-          <NormalButton label="전체 삭제" onClick={() => console.log('Button 2 clicked')} />
+          <NormalButton label="분석하기" onClick={ handleSendTextClick } />
+          <NormalButton label="전체 삭제" onClick={ handleDeleteTextClick } />
         </div>
     </div>
   );
