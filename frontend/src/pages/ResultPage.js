a9ee0ed9df8fd73e82ccdc8cf3c1ec55cef7ac91
Nbecause <thdduddn9696@gmail.com>
2024-02-26 14:29:58 +0900

    feat: Passing text

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-const ResultPage = ({ text }) => {
+const ResultPage = () => {
+  const location = useLocation();
+  const text = location.state.text;
+
   return (
     <div>
       <h1>Result Page</h1>
-      <p>Text from App component: {text}</p>
+      <p>본문 내용: {text}</p>
       {/* Add your content here */}
     </div>
   );
