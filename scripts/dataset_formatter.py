aca7d7f740c4d6b35d6acf34903416ac0f3754ae
exena <exena01@gmail.com>
2024-02-29 08:48:53 +0900

    feat: 데이터셋의 칼럼의 이름을 바꿔서 csv 파일로 내보냄

--- scripts/dataset_formatter.py
+++ scripts/dataset_formatter.py
@@ -3,7 +3,7 @@ import argparse
 
 # 옵션 arg 받기
 parser = argparse.ArgumentParser()
-parser.add_argument('--csvout', help='추출할 csv 파일 이름')
+parser.add_argument('--csvout', help='추출할 csv 파일 이름(확장자 빼고)')
 parser.add_argument('--subset', default='train', help='train, test, 혹은 valid ')
 parser.add_argument('--column', default='paragraph', help='지문 칼럼의 이름')
 parser.add_argument('--dataset', default='lmqg/qg_koquad', help='허깅페이스에서 가져올 데이터셋 이름')
@@ -15,7 +15,7 @@ dataset = load_dataset(args.dataset)
 #판다스 데이터프레임으로 변환
 train_data = dataset[args.subset].to_pandas()
 
-train_data = train_data.rename(columns={args.column: 'paragraph'})
+train_data = train_data.rename(columns={args.column: 'context'})
 
 # csv 파일로 추출
 if(args.csvout is not None):
