7f08b57d44447a84a6b6d657d7d1db4510f8d7e4
hyeming00 <han20han@khu.ac.kr>
2024-03-04 10:43:58 +0900

    feat: custom dataset

---
+++ data.py
@@ -0,0 +1,54 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import pickle as pickle
+from tqdm import tqdm
+from transformers import AutoTokenizer
+import numpy as np
+
+class CustomDataset(Dataset):
+
+    def __init__(self, dataset, tokenizer, max_len, ignore_index=-100):
+        self.dataset = dataset        
+        self.max_len = max_len
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        self.len = len(self.dataset)
+        self.pad_index = self.tokenizer.pad_token_id
+        self.ignore_index = ignore_index
+
+    def add_padding_data(self, inputs):
+        if len(inputs) < self.max_len:
+            pad = np.array([self.pad_index] *(self.max_len - len(inputs)))
+            inputs = np.concatenate([inputs, pad])
+        else:
+            inputs = inputs[:self.max_len]
+
+        return inputs
+    
+    def add_ignored_data(self, inputs):
+        if len(inputs) < self.max_len:
+            pad = np.array([self.ignore_index] *(self.max_len - len(inputs)))
+            inputs = np.concatenate([inputs, pad])
+        else:
+            inputs = inputs[:self.max_len]
+
+        return inputs
+    
+    def __getitem__(self, idx):
+        instance = self.dataset.iloc[idx]
+        input_ids = self.tokenizer.encode(instance['context'])
+        input_ids = self.add_padding_data(input_ids)
+
+        label_ids = self.tokenizer.encode(instance['question'])
+        label_ids.append(self.tokenizer.eos_token_id)
+        dec_input_ids = [self.tokenizer.eos_token_id]
+        dec_input_ids += label_ids[:-1]
+        dec_input_ids = self.add_padding_data(dec_input_ids)
+        label_ids = self.add_ignored_data(label_ids)
+
+        return {'input_ids': torch.tensor(input_ids, dtype=torch.long),
+                'decoder_input_ids': torch.tensor(dec_input_ids, dtype=torch.long),
+                'labels': torch.tensor(label_ids, dtype=torch.long)}
+
+    def __len__(self):
+        return self.len
