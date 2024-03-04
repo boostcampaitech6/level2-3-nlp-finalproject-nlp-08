78929c456a1680ea9ae0e0e5c014bc5c8ecb5cdd
exena <exena01@gmail.com>
2024-03-04 13:57:21 +0900

    feat: 몇몇 칼럼 추가

--- scripts/dataset_formatter.py
+++ scripts/dataset_formatter.py
@@ -20,47 +20,52 @@ dataset = load_dataset(args.dataset)
 train_data = dataset[args.subset].to_pandas()
 
 # 데이터셋에 따라서 다른 처리를 해줘야 한다.
+# KorQuad 1.0의 경우
 if args.dataset == 'squad_kor_v1':
-    col_name_dict = { 'answer' : 'answer', 'question':'question', 'context':'context',
+    col_name_dict = { 'id': 'id', 'answer' : 'answer', 'question':'question', 'context':'context',
                       'question_level':'question_level', 'question_type':'question_type',
-                      'answer_type':'answer_type', 'answer_start':'answer_start',
-                      'clue_start':'clue_start', 'clue_end':'clue_end' }
+                      'answer_type':'answer_type', 'answer_start':'answer_start', 'answer_end':'answer_end',
+                      'clue_text':'clue_text', 'clue_start':'clue_start', 'clue_end':'clue_end' }
     # 칼럼 이름 바꾸거나 생성
     train_data = change_column_name(col_name_dict, train_data)
     
-    # answer, answer_start, clue_start, clue_end 칼럼에 값 넣어주기(토큰단위가 아닌 글자단위 인덱스)
+    # answer, answer_start, answer_end, clue_text, clue_start, clue_end 칼럼에 값 넣어주기(토큰단위가 아닌 글자단위 인덱스)
     for idx, row in tqdm(train_data.iterrows()):
         train_data.loc[idx,'answer'] = row.answers['text'][0]
-        answer_start = train_data.loc[idx,'answer_start'] = row.answers['answer_start'][0]
-        _, start_idx, end_idx = find_sentence_at_index(row.context, answer_start)
+        train_data.loc[idx,'answer_start'] = answer_start = row.answers['answer_start'][0]
+        train_data.loc[idx,'answer_end'] = answer_start + len(row.answers['text'][0])
+        clue_text, start_idx, end_idx = find_sentence_at_index(row.context, answer_start)
+        train_data.loc[idx,'clue_text'] = clue_text
         train_data.loc[idx,'clue_start'] = start_idx
         train_data.loc[idx,'clue_end'] = end_idx
 
     # question_level, question_type, answer_type도 값을 넣어줄 수 있다면 넣어주자.
     pass
-    # 불필요한 열 삭제 id, title, answers
-    train_data.drop(['id', 'title', 'answers'], axis=1, inplace=True)
+    # 불필요한 열 삭제 title, answers
+    train_data.drop(['title', 'answers'], axis=1, inplace=True)
 
+# QG_Koquad의 경우
 if args.dataset == 'lmqg/qg_koquad':
-    col_name_dict = { 'answer' : 'answer', 'question':'question', 'paragraph':'context',
+    col_name_dict = { 'paragraph_id':'id', 'answer' : 'answer', 'question':'question', 'paragraph':'context',
                       'question_level':'question_level', 'question_type':'question_type',
-                      'answer_type':'answer_type', 'answer_start':'answer_start',
-                      'clue_start':'clue_start', 'clue_end':'clue_end' }
+                      'answer_type':'answer_type', 'answer_start':'answer_start', 'answer_end':'answer_end',
+                      'sentence':'clue_text', 'clue_start':'clue_start', 'clue_end':'clue_end' }
     # 칼럼 이름 바꾸거나 생성
     train_data = change_column_name(col_name_dict, train_data)
     
-    # answer_start, clue_start, clue_end 칼럼에 값 넣어주기(토큰단위가 아닌 글자단위 인덱스)
+    # answer_start, answer_end, clue_start, clue_end 칼럼에 값 넣어주기(토큰단위가 아닌 글자단위 인덱스)
     for idx, row in tqdm(train_data.iterrows()):
         clue_start = row.paragraph_sentence.find('<hl>')
         if clue_start != -1:
             train_data.loc[idx,'clue_start'] = clue_start
-            train_data.loc[idx,'clue_end'] = clue_start + len(row.sentence)
-            train_data.loc[idx,'answer_start'] = clue_start + row.sentence.find(row.answer)
+            train_data.loc[idx,'clue_end'] = clue_start + len(row.clue_text)
+            train_data.loc[idx,'answer_start'] = answer_start = clue_start + row.clue_text.find(row.answer)
+            train_data.loc[idx,'answer_end'] = answer_start + len(row.answer)
 
     # question_level, question_type, answer_type도 값을 넣어줄 수 있다면 넣어주자.
     pass
-    # 불필요한 열 삭제 paragraph_question, sentence, sentence_answer, paragraph_answer, paragraph_sentence, paragraph_id
-    train_data.drop(['paragraph_question', 'sentence', 'sentence_answer', 'paragraph_answer', 'paragraph_sentence', 'paragraph_id'], axis=1, inplace=True)
+    # 불필요한 열 삭제 paragraph_question, sentence_answer, paragraph_answer, paragraph_sentence
+    train_data.drop(['paragraph_question', 'sentence_answer', 'paragraph_answer', 'paragraph_sentence'], axis=1, inplace=True)
 
 # csv 파일로 추출
 if args.csvout is not None:
