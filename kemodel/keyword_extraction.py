efd49f9d0b6db1769576c757cff4b1fa15a5cedb
hyeming00 <han20han@khu.ac.kr>
2024-03-07 14:53:01 +0900

    fix: define arguments type

--- kemodel/keyword_extraction.py
+++ kemodel/keyword_extraction.py
@@ -5,6 +5,7 @@ from transformers import BertModel
 from collections import defaultdict
 from tqdm import tqdm
 import argparse
+import os
 
 def keyword_extraction(context, kw_model, num_to_gen=5, stop_words = None, n_gram = 5, use_maxsum=True, nr_candidates=10, use_mmr=True, diversity=0.5):
 
@@ -29,10 +30,11 @@ def remove_tag(context):
     return final_context
 
 if __name__:
+
     # 데이터 불러오기
+    print('Loading Data')
     DATA_DIR = '../../data/기술과학_train_ver2.csv'
     train_df = pd.read_csv(DATA_DIR)
-    print('Loaded Data')
 
     temp_df = train_df[(train_df['answer_type'] == '절차(방법)형') | (train_df['answer_type'] == '정답경계추출형')| (train_df['answer_type'] == '다지선다형')]
     temp_df = temp_df[['id', 'context', 'answer_type', 'answer']]
@@ -58,18 +60,20 @@ if __name__:
     # KeyBERT options
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', required=False, default='skt/kobert-base-v1', help='모델 이름')
-    parser.add_argument('--num_to_gen', required=False, default=3, help='생성할 keyword 수')
-    parser.add_argument('--n_gram', required=False, default=5, help='keyword의 max n gram')
+    parser.add_argument('--num_to_gen', required=False, type=int, default=3, help='생성할 keyword 수')
+    parser.add_argument('--n_gram', required=False, type=int, default=5, help='keyword의 max n gram')
     parser.add_argument('--use_maxsum', required=True, help='다양성 방법1')
-    parser.add_argument('--nr_candidates',required=False, help='use_maxsum=True일 경우 고려할 대상 개수')
+    parser.add_argument('--nr_candidates',required=False, type=int, help='use_maxsum=True일 경우 고려할 대상 개수')
     parser.add_argument('--use_mmr', required=True, help='다양성 방법2')
-    parser.add_argument('--diversity', required=False, default=0.5, help='use_mmr=True할 경우 다양성을 얼마나 줄건지(숫자 클수록 다양성 커짐)')
+    parser.add_argument('--diversity', required=False, type=float, default=0.8, help='use_mmr=True할 경우 다양성을 얼마나 줄건지(숫자 클수록 다양성 커짐)')
     args = parser.parse_args()
     
     model = BertModel.from_pretrained(args.model_name)
     kw_model = KeyBERT(model)
     print(args.model_name)
-        
+    print(args.diversity)
+    print(type(args.diversity))
+
     new_data = []
     for _, data in tqdm(docs_df.iterrows(), desc='keyword extraction', total = len(docs_df)):
         id = data['id']
@@ -88,9 +92,12 @@ if __name__:
     
     # 점수 계산
     score = 0
-    for _, data in keyword_df.iterrows():
+    for _, data in keyword_df[:3].iterrows():
         id = data['id']
         for keyword in data['keyword']:
             print(keyword, answer_df[answer_df['id']==id]['answer'])
             if keyword in answer_df[answer_df['id']==id]['answer']:
                 score += 1
+    score_list = [score, args.model_name, args.num_to_gen, args.n_gram, args.use_maxsum, args.nr_candidate, args.use_mmr, args.diversity]
+    score_df = pd.DataFrame(score_list, columns=['score', 'model_name', 'num_to_gen', 'n_gram', 'use_maxsum', 'nr_candidate', 'use_mmr', 'diversity'])
+    
