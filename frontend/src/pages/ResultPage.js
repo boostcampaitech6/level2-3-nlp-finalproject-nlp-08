d8411722ef68f01d0e6dceafda055ffc43b33c35
Yeongu Song <139039225+Nbecause@users.noreply.github.com>
2024-03-12 21:53:53 +0900

    beta version 0.4.0 (#17)
    * feat: like/dislike button
    
    * feat: prevent duplicate feedback
    
    * feat: docker compose

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import FixedTextBox from "../components/FixedTextBox";
 import QuestionBox from "../components/QuestionBox";
 import { IoArrowUndoCircle } from "react-icons/io5";
+import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
 import "./ResultPage.css";
 
 const ResultPage = () => {
@@ -25,8 +27,10 @@ const ResultPage = () => {
   const questions = dummyquestions;
   const answers = dummyanswers;
   const keywords = ["한국은행", "구조 개혁", "사회적 타협", "패스트 팔로어", "골든 타임"]
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/feedback";
   console.log("Questions:", questions);
   console.log("Answers:", answers);
+  const [submittedIssues, setSubmittedIssues] = useState(new Set());
 
   const handleButtonClick = (clickedText) => {
     console.log(`Button clicked in ResultPage: ${clickedText}`);
@@ -57,6 +61,27 @@ const ResultPage = () => {
     navigate("/");
   };
 
+  const handleFeedbackButtonClick = async (isLike) => {
+    const currentQuestionAnswer = `${questions[currentQuestionIndex]}-${answers[currentQuestionIndex]}`;
+
+    if (!submittedIssues.has(currentQuestionAnswer)) {
+    try {
+        const response = await axios.post(API_URL, {
+          question: questions[currentQuestionIndex],
+          answer: answers[currentQuestionIndex],
+          context: text,
+          like: isLike,
+        });
+        console.log('Feedback sent successfully:', response.data);
+        setSubmittedIssues(new Set([...submittedIssues, currentQuestionAnswer]));
+      } catch (error) {
+        console.error('Error sending feedback:', error);
+      }
+    } else {
+      console.log('Issue already submitted as feedback');
+    };
+  };
+
   return (
     <div>
       <header className="App-header">
@@ -77,6 +102,11 @@ const ResultPage = () => {
           isCorrect={isCorrect} 
           isAllCorrect={isAllCorrect}
         />
+      </div>
+
+      <div className="like-dislike-area">
+        <FaRegThumbsUp className="likeFeedbackButton" onClick={() => handleFeedbackButtonClick(true)} />
+        <FaRegThumbsDown className="disLikeFeedbackButton" onClick={() => handleFeedbackButtonClick(false)} />
       </div>
     </div>
   );
