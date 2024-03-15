53dad2bb09fd0bd0681000b7c86e44487cc2c06c
hyeming00 <han20han@khu.ac.kr>
2024-03-15 17:25:37 +0900

    feat: only noun keywords

--- backend/src/model_utils.py
+++ backend/src/model_utils.py
@@ -2,17 +2,21 @@ from loguru import logger
 from typing import Optional, List
 
 import torch
+import re
+from konlpy.tag import Kkma
 
 from dependencies import ml_models
 
 def extract_keywords(context: str, num_to_gen: int) -> List[str]:
     model = ml_models['ke_model']
-
     keywords = model.extract_keywords(context, 
                                       keyphrase_ngram_range=(1, 2), 
                                       stop_words=None, 
                                       top_n=num_to_gen)
+
     keywords = [k[0] for k in keywords]
+    keywords = [extracts_nouns(i) for i in keywords if i != None]
+    keywords = [i for i in keywords if i in context]
 
     return keywords
 
@@ -29,3 +33,13 @@ def generate_question(context: str, answer: Optional[str]) -> str:
     generated_question = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
 
     return generated_question
+
+def extracts_nouns(keyword):
+    kkma = Kkma()
+    for k in keyword.split():
+        temp_keyword = ''
+        for word, pos in kkma.pos(k):
+            if pos.startswith('N') and pos !='NP':
+                temp_keyword += word
+        if len(temp_keyword) != 0:
+            return temp_keyword
