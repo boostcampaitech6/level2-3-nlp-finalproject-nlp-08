811d83062439f7de823959d6432c982ce0f8d009
hyeming00 <han20han@khu.ac.kr>
2024-03-17 14:57:28 +0900

    feat: change korean library(konlpy -> kiwi)

--- backend/src/model_utils.py
+++ backend/src/model_utils.py
@@ -3,20 +3,21 @@ from typing import Optional, List
 
 import torch
 import re
-from konlpy.tag import Kkma
+from kiwipiepy import Kiwi
 
 from dependencies import ml_models
 
 def extract_keywords(context: str, num_to_gen: int) -> List[str]:
     model = ml_models['ke_model']
     keywords = model.extract_keywords(context, 
-                                      keyphrase_ngram_range=(1, 2), 
+                                      keyphrase_ngram_range=(1, 1), 
                                       stop_words=None, 
-                                      top_n=num_to_gen)
+                                      top_n=num_to_gen,
+                                      use_mmr=True,
+                                      diversity=0.1)
 
     keywords = [k[0] for k in keywords]
-    keywords = [extracts_nouns(i) for i in keywords if i != None]
-    keywords = [i for i in keywords if i in context]
+    keywords = [extracts_nouns(i) for i in keywords if extracts_nouns(i) != None and extracts_nouns(i) in context]
 
     return keywords
 
@@ -34,12 +35,15 @@ def generate_question(context: str, answer: Optional[str]) -> str:
 
     return generated_question
 
+from kiwipiepy import Kiwi
 def extracts_nouns(keyword):
-    kkma = Kkma()
+    kiwi = Kiwi()
+    final_keyword = []
     for k in keyword.split():
         temp_keyword = ''
-        for word, pos in kkma.pos(k):
-            if pos.startswith('N') and pos !='NP':
-                temp_keyword += word
-        if len(temp_keyword) != 0:
-            return temp_keyword
+        for i in kiwi.tokenize(k):
+            if i.tag.startswith('N') or i.tag=='SN' and i.tag !='NP':
+                temp_keyword += i.form
+        final_keyword.append(temp_keyword)
+    if len(final_keyword) != 0:
+        return ' '.join(final_keyword)
