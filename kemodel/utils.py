2a1c772a656cbf55074df00c76a62883a98caa13
hyeming00 <han20han@khu.ac.kr>
2024-03-20 23:18:30 +0900

    feat: change noun extraction library(konlpy to kiwipiepy)

--- kemodel/utils.py
+++ kemodel/utils.py
@@ -1,5 +1,6 @@
 import re
 from konlpy.tag import Kkma
+from kiwipiepy import Kiwi
 
 def preprocessing_data(context):
     '''
@@ -11,11 +12,14 @@ def preprocessing_data(context):
     return final_context
 
 def extracts_nouns(keyword):
-    kkma = Kkma()
+    kiwi = Kiwi()
+    final_keyword = []
     for k in keyword.split():
         temp_keyword = ''
-        for word, pos in kkma.pos(k):
-            if pos.startswith('N'):
-                temp_keyword += word
-        if len(temp_keyword) != 0:
-            return temp_keyword
+        for i in kiwi.tokenize(k):
+            if i.tag.startswith('N') or i.tag=='SN' and i.tag !='NP':
+                temp_keyword += i.form
+        final_keyword.append(temp_keyword)
+    if len(final_keyword) != 0:
+        return ' '.join(final_keyword)
+
